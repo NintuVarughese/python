b3aa68f8f5aa732f327207c3af8f473827142c29
nintumariya02@gmail.com <nintuvarughese02@gmail.com>
2024-06-10 16:08:00 +0530

    pyramid

---
+++ pyramid.py
@@ -0,0 +1,16 @@
+def print_pyramid(levels):
+    for i in range(levels):
+        
+        for j in range(levels - i - 1):
+            print(" ", end="")
+        
+        
+        for k in range(2 * i + 1):
+            print("*", end="")
+        
+        
+        print()
+
+
+levels = int(input("Enter the number of levels for the pyramid: "))
+print_pyramid(levels)
